729fb1c99bf24554f3b290e18ec99325a22f9d54
melaeke <smelaeke22@gmail.com>
2018-02-01 12:38:39 +0300

    Empty index page and simple routing

--- server.js
+++ server.js
@@ -7,6 +7,15 @@ const app = express();
 
 mongoose.connect('mongodb://localhost:27017/aklesia_db');
 
+//Parsers
+app.use(bodyParser.json());
+app.use('/node_modules',express.static(__dirname+"/node_modules"));
+
+//send index.html for the home page
+app.get('/',(req,res)=>{
+    res.sendFile(path.join(__dirname,'index.html'));
+});
+
 const port = process.env.PORT || '3000';
 app.set('port',port);
 const server = http.createServer(app);
