b6ca60690b9aedf0c6f1147cd83aba0cedf04a51
melaeke <smelaeke22@gmail.com>
2018-02-12 16:48:20 +0300

    support for articles

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -14,5 +14,25 @@ module.exports = (app)=>{
         .put(usersController.update)
         .delete(usersController.delete);
 
+    var articlesTypeController= require('../controllers/articleTypesController');
+    app.route('/api/articleTypes')
+        .get(articlesTypeController.getAll)
+        .post(articlesTypeController.create);
+
+    app.route('/api/articleTypes/:articelTypeId')
+        .get(articlesTypeController.getOne)
+        .put(articlesTypeController.update)
+        .delete(articlesTypeController.delete);
+
+    //articles
+    var articlesController=require('../controllers/articlesController');
+    app.route('/api/articles')
+        .get(articlesController.getAll)
+        .post(articlesController.create);
+    
+    app.route('/api/articles/:articleId')
+        .get(articlesController.getOne)
+        .put(articlesController.update)
+        .delete(articlesController.delete);
     
 }
