3191e062cefb88bc234f2978735abb15f2cedb58
melaeke <smelaeke22@gmail.com>
2018-02-13 11:53:55 +0300

    support for angular and angular routing

--- app/app.js
+++ app/app.js
@@ -1,3 +1,26 @@
-(function(){
-    angular.module("aklesia",[]);
-}());
+var app = angular.module('aklesia',['ngRoute']);
+
+app.config(($routeProvider)=>{
+    $routeProvider.when("/",{
+        templateUrl:"app/views/index.html"
+    }).when("/home",{
+        templateUrl:"app/views/index.html"
+    }).when("/pages",{
+        templateUrl: "app/views/pages.html",
+        controller: "app/controllers/pages"
+    }).when("/contents",{
+        templateUrl: "app/views/contents.html",
+        controller: "app/controllers/contents"
+    }).when("/notifications",{
+        templateUrl: "app/views/notifications.html",
+        controller: "app/controllers/contents"
+    })
+})
+
+
+app.controller('itemController',($scope)=>{
+    $scope.first=1;
+    $scope.second=1;
+
+    $scope.person.name="New Name";
+})
