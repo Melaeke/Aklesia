7b09b3b53b0cacdb8488bf2b22b010457b51019c
melaeke <smelaeke22@gmail.com>
2018-02-21 13:59:04 +0300

    Only UI support for editing and creating everything

--- app/controllers/contents-controller.js
+++ app/controllers/contents-controller.js
@@ -10,7 +10,16 @@ mainApp.controller('contentsController',function ($scope){
     $scope.model=[];
     $scope.model.allCatagories=["Page","Article","User"];
     $scope.model.allArticleTypes=["Text","Picture","Video","Audio"];
-    $scope.model.allPages=["page1","page2","page3","page4"];
+    $scope.model.allPages=[{
+        "name":"page1",
+        "numberOfArticles":15},
+        {"name":"page2",
+        "numberOfArticles":20},
+        {"name":"page3",
+        "numberOfArticles":15},
+        {"name":"page4",
+        "numberOfArticles":12}
+    ];
     $scope.model.allUsers=[
         {"firstName":"Abebe","userType":"Admin","email":"abc@yahoo.com"},
         {"firstName":"Kebede","userType":"normal","email":"kebede_chala2@yahoo.com"},
@@ -20,15 +29,27 @@ mainApp.controller('contentsController',function ($scope){
     $scope.model.allUserTypes=["normal","pageAdmin","Admin"];
 
     $scope.action=(actionType)=>{
+        $scope.model.actionType=actionType;
+        $scope.model.user=[];
+        $scope.model.page=[];
+        $scope.model.article=[];
         $scope.model.selectedAction=true;
 
         if(actionType === 'createNew'){
             if($scope.model.selectedCatagory === 'User'){
                 $scope.model.selectedUser=true;
+            }else if($scope.model.selectedCatagory==='Page'){
+                $scope.model.selectedPage=true;
+            }else if($scope.model.selectedCatagory=== 'Article'){
+                $scope.model.selectedArticle=true;
             }
         }else if(actionType === 'editExisting'){
             if($scope.model.selectedCatagory === 'User'){
                 $scope.model.selectedUser=false;
+            }else if($scope.model.selectedCatagory==='Page'){
+                $scope.model.selectedPage=false;
+            }else if($scope.model.selectedCatagory=== 'Article'){
+                $scope.model.selectedArticle=false;
             }
         }
 
@@ -40,6 +61,18 @@ mainApp.controller('contentsController',function ($scope){
         console.log(user);
     }
 
+    $scope.selectPage=(page)=>{
+        $scope.model.page=page;
+        $scope.model.selectedPage=true;
+        console.log($scope.model.page);
+    }
+
+    $scope.selectArticle=(article)=>{
+        $scope.model.article=article;
+        $scope.model.selectedArticle=true;
+        console.log($scope.model.selectedArticle);
+    }
+
     $scope.$watch('model.action',function(){
         console.log("action",$scope.model.action);
     });
@@ -49,6 +82,8 @@ mainApp.controller('contentsController',function ($scope){
         //when changing catagory, reinitialize everything.
         $scope.model.action="";
         $scope.model.selectedAction=false;
+        $scope.model.selectedPage=false;
+
 
         console.log("Selected Catagory",$scope.model.selectedCatagory);
     });
