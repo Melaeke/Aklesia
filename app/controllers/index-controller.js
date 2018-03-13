63d419fea643dcc10711ced9f22cdba4115f1b42
melaeke <smelaeke22@gmail.com>
2018-03-14 00:04:02 +0300

    angular support for headers

---
+++ app/controllers/index-controller.js
@@ -0,0 +1,29 @@
+
+var mainApp = angular.module('aklesia');
+
+mainApp.controller('indexController',function ($scope,$http,networkService){
+    
+    initializeHeader($scope,networkService)
+});
+
+var initializeHeader=($scope,networkService)=>{
+    $scope.model=[];
+    $scope.model.allPages=[];
+    networkService.getFromServer("api/pages?fields=_id,name")
+        .then((allPages)=>{
+            //this is a success
+            $scope.model.allPages=allPages;
+        },(error)=>{
+            //this is an error
+            console.error("The request returned with an error",error);
+    });
+
+    networkService.getFromServer("api/articleTypes?fields=_id,name")
+        .then((allArticleTypes)=>{
+            //this is a success
+            $scope.model.allArticleTypes=allArticleTypes;
+        },(error)=>{
+            //this is an error
+            console.error("The request returned with an error",error);
+    });
+}
